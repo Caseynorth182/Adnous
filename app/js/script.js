8c963d14026b2886dceff2e0d02fa2322f20bfad
Alexander <Caseynorth182@gmail.com>
2020-08-08 00:55:32 +0300

    add subpromo, how it works pages

--- app/js/script.js
+++ app/js/script.js
@@ -1,1 +1,11 @@
-$(function () {});
+$(function() {
+    $('.fancybox').fancybox({
+        padding: 0, //убираем отступ
+        helpers: {
+            overlay: {
+                locked: false // отключаем блокировку overlay
+            }
+        }
+    });
+
+});
--- gulpfile.js
+++ gulpfile.js
@@ -7,7 +7,7 @@ let gulp = require("gulp"),
     uglify = require("gulp-uglify"),
     cssmin = require("gulp-cssmin");
 
-gulp.task("sass", function () {
+gulp.task("sass", function() {
     return gulp
         .src("app/scss/**/*.scss")
         .pipe(
@@ -33,46 +33,46 @@ gulp.task("sass", function () {
         );
 });
 
-gulp.task("script", function () {
+gulp.task("script", function() {
     return gulp
         .src([
             "node_modules/jquery/dist/jquery.js",
             "node_modules/slick-carousel/slick/slick.js",
-            /* "node_modules/magnific-popup/dist/jquery.magnific-popup.js",
-            "node_modules/mixitup/dist/mixitup.js",
-            "node_modules/rateyo/src/jquery.rateyo.js",
-            "node_modules/ion-rangeslider/js/ion.rangeSlider.js",
+            /*   "node_modules/magnific-popup/dist/jquery.magnific-popup.js", */
+            /* "node_modules/mixitup/dist/mixitup.js", */
+            /*  "node_modules/rateyo/src/jquery.rateyo.js", */
+            /*  "node_modules/ion-rangeslider/js/ion.rangeSlider.js", */
             "node_modules/@fancyapps/fancybox/dist/jquery.fancybox.js",
-            "node_modules/jquery-form-styler/dist/jquery.formstyler.js", */
+            /* "node_modules/jquery-form-styler/dist/jquery.formstyler.js",  */
         ])
         .pipe(concat("libs.min.js"))
         .pipe(uglify())
         .pipe(gulp.dest("app/js"));
 });
-gulp.task("style", function () {
+gulp.task("style", function() {
     return gulp
         .src([
             "node_modules/slick-carousel/slick/slick.css",
             /*   "node_modules/magnific-popup/dist/magnific-popup.css", */
             "node_modules/normalize.css/normalize.css",
-            /*      "node_modules/rateyo/src/jquery.rateyo.css",
-            "node_modules/ion-rangeslider/css/ion.rangeSlider.css",
+            /* "node_modules/rateyo/src/jquery.rateyo.css", */
+            /* "node_modules/ion-rangeslider/css/ion.rangeSlider.css", */
             "node_modules/@fancyapps/fancybox/dist/jquery.fancybox.css",
-            "node_modules/jquery-form-styler/dist/jquery.formstyler.css", */
+            /* "node_modules/jquery-form-styler/dist/jquery.formstyler.css",  */
         ])
         .pipe(concat("libs.min.css"))
         .pipe(cssmin())
         .pipe(gulp.dest("app/css"));
 });
 
-gulp.task("html", function () {
+gulp.task("html", function() {
     return gulp.src("app/*.html").pipe(
         browserSync.reload({
             stream: true,
         })
     );
 });
-gulp.task("js", function () {
+gulp.task("js", function() {
     return gulp.src("app/js/*.js").pipe(
         browserSync.reload({
             stream: true,
@@ -80,7 +80,7 @@ gulp.task("js", function () {
     );
 });
 
-gulp.task("browser-sync", function () {
+gulp.task("browser-sync", function() {
     browserSync.init({
         server: {
             baseDir: "app/",
@@ -88,7 +88,7 @@ gulp.task("browser-sync", function () {
     });
 });
 
-gulp.task("watch", function () {
+gulp.task("watch", function() {
     gulp.watch("app/scss/**/*.scss", gulp.parallel("sass"));
     gulp.watch("app/*.html", gulp.parallel("html"));
     gulp.watch("app/js/*.js", gulp.parallel("js"));
