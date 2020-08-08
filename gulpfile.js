18d8225a1f053b7f000a5a5e375bb47ff5682965
Alexander <Caseynorth182@gmail.com>
2020-08-08 20:21:56 +0300

    add case page

--- gulpfile.js
+++ gulpfile.js
@@ -43,6 +43,7 @@ gulp.task("script", function() {
             /*  "node_modules/rateyo/src/jquery.rateyo.js", */
             /*  "node_modules/ion-rangeslider/js/ion.rangeSlider.js", */
             "node_modules/@fancyapps/fancybox/dist/jquery.fancybox.js",
+            "node_modules/fontawesome-pro-5.8.2-web/js/all.js",
             /* "node_modules/jquery-form-styler/dist/jquery.formstyler.js",  */
         ])
         .pipe(concat("libs.min.js"))
@@ -58,6 +59,7 @@ gulp.task("style", function() {
             /* "node_modules/rateyo/src/jquery.rateyo.css", */
             /* "node_modules/ion-rangeslider/css/ion.rangeSlider.css", */
             "node_modules/@fancyapps/fancybox/dist/jquery.fancybox.css",
+            "node_modules/fontawesome-pro-5.8.2-web/css/all.css",
             /* "node_modules/jquery-form-styler/dist/jquery.formstyler.css",  */
         ])
         .pipe(concat("libs.min.css"))
