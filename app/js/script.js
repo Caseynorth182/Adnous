ad232d16cd2e6e4c64a5f9764eb8b787a4abc107
Alexander <Caseynorth182@gmail.com>
2020-08-12 00:35:47 +0300

    add recomended, expert-blog, companies section and some javascript

--- app/js/script.js
+++ app/js/script.js
@@ -14,6 +14,7 @@ $(function() {
         slidesToShow: 3,
         slidesToScroll: 2,
         arrows: false,
+        pauseOnHover: true,
         autoplay: true,
         dots: true,
     });
@@ -23,11 +24,20 @@ $(function() {
         infinite: true,
         slidesToShow: 3,
         slidesToScroll: 2,
+        pauseOnHover: true,
         arrows: false,
         autoplay: true,
         dots: true,
     });
-
+    //COMPANIES SLIDER
+    $('.companies__slider').slick({
+        infinite: true,
+        slidesToShow: 6,
+        slidesToScroll: 1,
+        pauseOnHover: true,
+        arrows: false,
+        autoplay: true,
+    });
     //SCROLL MODIFIER
     (function($) {
         $(window).on('load', function() {
@@ -38,6 +48,30 @@ $(function() {
         });
     })(jQuery);
 
+    //VIDEO BUTTON CONTROLL -- LEFT SIDE VIDEO
+    $('#left-video__btn').on('click', function() {
+        $('.video__link--left').click()
+    });
+    //VIDEO BUTTON CONTROLL -- RIGHT SIDE VIDEO
+    $('#right-video__btn').on('click', function() {
+        $('.video__link--right').click()
+    });
 
-
+    //UPBUTTON 
+    $(window).scroll(function() {
+        if ($(this).scrollTop() > 100) {
+            if ($('#upbutton').is(':hidden')) {
+                $('#upbutton').css({
+                    opacity: 1
+                }).fadeIn('slow');
+            }
+        } else {
+            $('#upbutton').stop(true, false).fadeOut('fast');
+        }
+    });
+    $('#upbutton').click(function() {
+        $('html, body').stop().animate({
+            scrollTop: 0
+        }, 300);
+    });
 });
