bd079554bab6d7f6952e7bb4efd0764145290b31
Alexander Baranov <Caseynorth182@gmail.com>
2020-08-21 00:05:33 +0300

    strat addaptive

--- app/js/script.js
+++ app/js/script.js
@@ -29,6 +29,27 @@ $(function() {
         }
     }
 
+    //BLOG HEADER SCICK MENU _ ON TABLETS AND PHONES
+    if ($(window).width() <= 600) {
+        var scrolled;
+        window.onscroll = function() {
+            scrolled = window.pageYOffset || document.documentElement.scrollTop;
+            if (scrolled > 60) {
+                $(".header").css({
+                    "background": " rgba(84, 128, 221, 0.8)"
+                })
+            }
+            if (60 > scrolled) {
+                $(".header").css({
+                    "background": " rgba(255, 255, 255, 0.1)"
+                })
+            }
+        }
+    }
+    /* else if ($(window).width() >= 992) {
+           $('.header__btns').appendTo(".header__inner");
+       } */
+
     //ACCORDION ON TOOLS PAGE
     $(".accord__trigger").on("click",
         function() {
@@ -39,24 +60,44 @@ $(function() {
     $('.supprot__slider').slick({
         infinite: true,
         slidesToShow: 3,
-        slidesToScroll: 2,
+        slidesToScroll: 1,
+        loop: true,
         arrows: false,
         pauseOnHover: true,
         autoplay: true,
         autoplaySpeed: 1000,
         dots: true,
+        responsive: [{
+            breakpoint: 900,
+            settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1,
+                autoplaySpeed: 2000,
+                fade: true,
+            }
+        }]
+
     });
 
     //PASS SLICK SLIDER
     $('.pass__slider').slick({
         infinite: true,
         slidesToShow: 3,
-        slidesToScroll: 2,
+        slidesToScroll: 1,
         pauseOnHover: true,
         arrows: false,
-        autoplay: true,
+        /*  autoplay: true, */
         autoplaySpeed: 1000,
         dots: true,
+        responsive: [{
+            breakpoint: 900,
+            settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1,
+                autoplaySpeed: 2000,
+
+            }
+        }]
     });
     //COMPANIES SLIDER
     $('.companies__slider').slick({
@@ -69,7 +110,7 @@ $(function() {
         autoplay: true,
     });
 
-    //SCROLL MODIFIER
+    //side-SCROLL MODIFIER
     (function($) {
         $(window).on('load', function() {
             $('el').mCustomScrollbar({
@@ -83,6 +124,24 @@ $(function() {
         $(this).closest('.recomended__inner').find('.video__link').click();
         $(this).closest('.review__inner').find('.video__link').click();
     });
+
+    //NOTE APPEND LOGIN BUTTONS FOR ADDAPTIVE
+    if ($(window).width() <= 991) {
+        $('.header__btns').appendTo(".header__menu");
+    } else if ($(window).width() >= 992) {
+        $('.header__btns').appendTo(".header__inner");
+    }
+    //NOTE BURGER MENU
+    $('.header__burger').on('click', function() {
+        $('.header__burger').toggleClass('active');
+        $('.header__menu').toggleClass('active');
+    })
+
+    //NOTE DROPDOWN ON CLICK "ЕЩЕ"
+    $(".menu__item--drop").click(function() {
+        $(".dropdown").toggleClass("active");
+    });
+    $('.menu').on('touchstart', function() {}); // - off custom hover effect
     //PAGINATION SCRIPT
     $('#pagination-container').pagination({
             dataSource: [1, 2, 3, 4, 5, 6, 7],
